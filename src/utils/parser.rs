70d5c53df8d6c8b841869274322fb7e80b60bf5b
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-03-06 22:43:09 +0800

    Removed pulldown_cmark for Parser trait

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -1,4 +1,30 @@
 // Utilities to parse .md files and returns them in useful objects.
-use pulldown_cmark::{Event, Parser, Options};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use crate::app::agenda::{Activity, Agenda};
+
+pub trait Parser<T> {
+    fn parse(filepath: &str) -> Result<Vec<T>, io::Error>;
+}
 
 pub struct AgendaParser;
+
+// Agenda file structure:
+// title: string
+// start: date with timezone
+// end: date with timezone (optional)
+// description: string (optional)
+// priority: has to match Priority enum
+impl Parser<Activity> for AgendaParser {
+    fn parse(filepath: &str) -> Result<Vec<Activity>, io::Error> {
+        let content = Vec::new();
+        let path = Path::new(filepath);
+        let file = File::open(&path)?;
+        let reader = io::BufReader::new(file);
+        for line in reader.lines(){
+            println!("{}", line?);
+        }
+        Ok(content)
+    }
+}
