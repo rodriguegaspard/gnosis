aa82606d3987f1ba0496cd7dbf979f42d8981cd6
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2025-03-27 14:16:17 +0800

    Removed ensure_path_exists in favor of read_file in init.rs

--- src/utils/init.rs
+++ src/utils/init.rs
@@ -1,17 +1,18 @@
-use std::fs::{self, File};
-use std::io::{self, ErrorKind};
+use std::fs;
+use std::io::{self, Read};
 use std::path::Path;
 
-pub fn ensure_file_exists(filepath: &str) -> io::Result<File> {
+pub fn read_file(filepath: &str) -> io::Result<String> {
     let path = Path::new(filepath);
     if let Some(parent) = path.parent() {
         fs::create_dir_all(parent)?;
     }
-    match File::open(&path) {
-        Ok(file) => Ok(file),
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            File::create(&path)
-        },
-        Err(error) => Err(error),
+    if !path.exists() {
+        fs::File::create(path)?;
+        Ok("".to_string())
+    } else {
+        let mut content = String::new();
+        fs::File::open(path)?.read_to_string(&mut content)?;
+        Ok(content)
     }
 }
